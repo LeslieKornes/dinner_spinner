f30915766ea2b6b79bc033c65d2c986be29f42ec
Leslie Kornes <lesliekornes@gmail.com>
2022-12-29 10:04:42 -0500

    remove test directory and add new and create actions and views

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,8 +4,28 @@ class MealsController < ApplicationController
     @meals = Meal.all
   end
 
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to meals_path, notice: "Meal successfully created!"
+    else
+      render :new
+    end
+  end
+
   def show
     @meal = Meal.find(params[:id])
+  end
+
+  private
+  
+  def meal_params
+    params.require(:meal).
+      permit(:name, :description, :link)
   end
 
 end
