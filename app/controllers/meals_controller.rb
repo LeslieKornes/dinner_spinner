d6d0d31a8104e02999256a44863fdacec21f4bfc
Leslie Kornes <lesliekornes@gmail.com>
2022-12-30 11:55:39 -0500

    add basic CRUD actions and views

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -11,14 +11,37 @@ class MealsController < ApplicationController
   def create
     @meal = Meal.new(meal_params)
     if @meal.save
+      # TODO: get Flash messages working (turbo now?)
       redirect_to meals_path, notice: "Meal successfully created!"
     else
       render :new
     end
   end
 
+  def edit
+    @meal = Meal.find(params[:id])
+  end
+
+  def update
+    # TODO: this will only be possible by Meal creator or group admin
+    @meal = Meal.find(params[:id])
+    if @meal.update(meal_params)
+      # TODO: get Flash messages working (turbo now?)
+      redirect_to meal_path(@meal), alert: "Meal successfully updated!"
+    else
+      render :edit
+    end
+  end
+
   def show
     @meal = Meal.find(params[:id])
+  end
+
+  def destroy
+    @meal = Meal.find(params[:id])
+    @meal.destroy
+    # look: status: :see_other is a Rails 7 feature (https://guides.rubyonrails.org/getting_started.html#deleting-an-article)
+    redirect_to meals_url, status: :see_other
   end
 
   private
